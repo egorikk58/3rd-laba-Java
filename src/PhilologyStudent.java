c2d37e24821bc70be0b0df9f6978c52900ee6ee9
egorikk58 <egorka.andreev765@gmail.com>
2024-10-06 20:12:24 +0300

    Добавил сеттеры геттеры для студентов философии

--- src/PhilologyStudent.java
+++ src/PhilologyStudent.java
@@ -6,21 +6,28 @@ public class PhilologyStudent extends Student {
     private String _specialization; // специализация (лингвистика, литература и т.д.)
     private String _favoriteAuthor; // любимый автор
     private String _languageOfStudy; // изучаемый язык
+    private String _thesisTopic; // тема диплома
+    private String _researchInterests; // исследовательские интересы
 
     public PhilologyStudent() {
         super();
         _specialization = null;
         _favoriteAuthor = null;
         _languageOfStudy = null;
+        _thesisTopic = null;
+        _researchInterests = null;
     }
 
     public PhilologyStudent(String fullname, String gender, int age, String countryofbirth, String phonenumber,
                             String studentID, int yearofstudy, int absences, BigDecimal gpa, String hasScholarship,
-                            String specialization, String favoriteAuthor, String languageOfStudy) {
+                            String specialization, String favoriteAuthor, String languageOfStudy,
+                            String thesisTopic, String researchInterests) {
         super(fullname, gender, age, countryofbirth, phonenumber, studentID, yearofstudy, absences, gpa, hasScholarship);
         this._specialization = specialization;
         this._favoriteAuthor = favoriteAuthor;
         this._languageOfStudy = languageOfStudy;
+        this._thesisTopic = thesisTopic;
+        this._researchInterests = researchInterests;
     }
 
     public String getSpecialization() {
@@ -47,24 +54,20 @@ public class PhilologyStudent extends Student {
         this._languageOfStudy = languageOfStudy;
     }
 
-    public void analyzeText() {
-        System.out.println("Analyzing a literary text.");
+    public String getThesisTopic() {
+        return _thesisTopic;
     }
 
-    public void translateText() {
-        System.out.println("Translating text from " + _languageOfStudy);
+    public void setThesisTopic(String thesisTopic) {
+        this._thesisTopic = thesisTopic;
     }
 
-    public void writeEssay() {
-        System.out.println("Writing an essay on " + _favoriteAuthor);
+    public String getResearchInterests() {
+        return _researchInterests;
     }
 
-    public void participateInConference() {
-        System.out.println("Participating in a philological conference.");
-    }
-
-    public void studyLiterature() {
-        System.out.println("Studying literature in the field of " + _specialization);
+    public void setResearchInterests(String researchInterests) {
+        this._researchInterests = researchInterests;
     }
 
     @Override
@@ -74,6 +77,8 @@ public class PhilologyStudent extends Student {
         info.add("Specialization: " + _specialization);
         info.add("Favorite Author: " + _favoriteAuthor);
         info.add("Language of Study: " + _languageOfStudy);
+        info.add("Thesis Topic: " + _thesisTopic);
+        info.add("Research Interests: " + _researchInterests);
         return info;
     }
 }
